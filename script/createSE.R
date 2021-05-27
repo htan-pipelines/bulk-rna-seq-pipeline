fac0efcd2655eb5afad81a033cff6ebbb63fc814
wmisch <60713190+wmisch@users.noreply.github.com>
2021-05-27 13:31:58 -0400

    Update createSE.R

--- script/createSE.R
+++ script/createSE.R
@@ -30,7 +30,7 @@ create_se <- function(input, tinfile, gtf, gene_file, isoform_file,sample.name,s
   output[["inputfile"]] <- data
   colnames(output[["inputfile"]]) <- paste(
     "rnaseqc", colnames(output[["inputfile"]]),sep="_")
-  )
+  
   # Get tab-delimited column names from first line of first file 
   output[["tinfile"]] <- c(readLines(tinfile, n=1), 
                            sapply(tinfile, scan, what="", n=1, sep="\n", skip=1, quiet=TRUE)) # Read tab-delimited contents of second lines of all files
